fbc78fbdb429d1b438f1fcf1d8d693e4c2e58096
kannkyo <15080890+kannkyo@users.noreply.github.com>
2021-11-06 09:23:24 +0900

    Fix can not upload

--- bin/upload.sh
+++ bin/upload.sh
@@ -2,6 +2,23 @@
 
 . ./bin/env.ini
 
-echo "upload ${PACKAGE_REFERENCE} to remote"
+FILE_NAME=`basename $0`
 
+while getopts t: OPT
+do
+  case $OPT in
+    "t" ) FLG_T="TRUE" ; CONAN_REGISTORY_URL="$OPTARG" ;;
+      * ) echo "Usage: $FILE_NAME [-t target_url]" 1>&2
+          exit 1 ;;
+  esac
+done
+
+if [ "$FLG_T" != "TRUE" ]; then
+  echo "[ERROR] please set target_url = http://url/to/conan/package/registory" 1>&2
+  exit 1
+fi
+
+echo "upload ${PACKAGE_REFERENCE} to ${CONAN_REGISTORY_URL}"
+
+conan remote add gitlab ${CONAN_REGISTORY_URL} --force
 conan upload ${PACKAGE_REFERENCE} --all --remote=gitlab --confirm --force
